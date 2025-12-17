ebf3e22dd7da70aae77e0f4cff78d87a535d65c5
Pintér Gábor <pinter.gabor@gmx.at>
2025-12-17 10:12:41 +0100

    Bump versions.

--- src/main/java/eu/pintergabor/crusher/datagen/ModItemTagProvider.java
+++ src/main/java/eu/pintergabor/crusher/datagen/ModItemTagProvider.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
@@ -123,7 +123,7 @@ public final class ModItemTagProvider extends FabricTagProvider.ItemTagProvider 
 	 */
 	private static @NotNull TagKey<Item> createCItemTag(String path) {
 		return TagKey.create(
-			Registries.ITEM, ResourceLocation.fromNamespaceAndPath("c", path));
+			Registries.ITEM, Identifier.fromNamespaceAndPath("c", path));
 	}
 
 	/**
